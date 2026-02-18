79b84c7760682363a372e171f7ff34dc293d6b6a
priyanshu4492 <priyanshudangi449244@gmail.com>
2026-02-18 22:48:27 +0530

    boxshadow

--- lib/pages/shapes.dart
+++ lib/pages/shapes.dart
@@ -10,24 +10,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
+    
     return MaterialApp(
       debugShowCheckedModeBanner: false,
 
       home: Scaffold(
         backgroundColor: const Color.fromARGB(255, 205, 244, 206),
+        appBar: AppBar(
+          leading: IconButton(onPressed: (){}, icon: Icon(Icons.menu)),
+          title: Center(child: Text("MYAPP" ),
+          
+          ),
+          backgroundColor: Colors.greenAccent,
+        ),
 
         body: Column(
           children: [
             Container(
-              width: double.infinity,
               height: 400,
+              width: double.infinity,
               decoration: BoxDecoration(
                 color: Colors.red,
                 borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(screenWidth * 0.15),
-                  bottomRight: Radius.circular(screenWidth * 0.15),
+                    bottomLeft: Radius.circular(screenWidth*0.15),
+                    bottomRight: Radius.circular(screenWidth*0.15),
                 ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.3),
+                    blurRadius: 20,
+                    spreadRadius: 5,
+                    offset: Offset(0, 10),
+                  )
+                ]
               ),
+              
+
             ),
           ],
         ),
